dc53239f5a2886fce5bfba3ee4231dc110c9fcc3
Prakarsh Upmanyu <prakarshupmanyu@prakarshs-mbp.lan>
2021-05-26 17:10:14 -0700

    Initial commit
    Signed-off-by: Prakarsh Upmanyu <prakarshupmanyu@prakarshs-mbp.lan>

---
+++ src/main/scala/consumer/StockConsumer.scala
@@ -0,0 +1,122 @@
+package consumer
+
+import scala.collection.JavaConverters._
+import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
+import org.apache.kafka.common.serialization.StringDeserializer
+
+import java.util.Properties
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.types.IntegerType
+import org.apache.spark.sql.functions.{avg, col, from_json}
+import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{ArrayType, StructType}
+import org.apache.spark.sql.DataFrame
+
+object StockConsumer extends App {
+  /*
+  val properties = new Properties()
+  properties.put("bootstrap.servers", BROKER_LIST)
+  properties.put("group.id", "stock-consumers")
+  properties.put("partition.assignment.strategy", "")
+  properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+  properties.put("key.deserializer", classOf[StringDeserializer])
+  properties.put("value.deserializer", classOf[StringDeserializer])
+
+  val kafkaConsumer = new KafkaConsumer[String, String](properties)
+  kafkaConsumer.subscribe(TOPIC)
+
+  while (true) {
+    val results = kafkaConsumer.poll(2000).asScala
+    println(results)
+    for ((topic, data) <- results) {
+      println(topic + " :: " + data)
+    }
+  }
+  */
+  val BROKER_LIST = "localhost:9092"
+  val TOPIC = "test"
+
+  val spark = SparkSession
+    .builder()
+    .appName("StockConsumer")
+    .getOrCreate()
+
+  val df = spark
+    .read
+    .format("kafka")
+    .option("kafka.bootstrap.servers", BROKER_LIST)
+    .option("startingOffsets", "earliest")
+    .option("subscribe", TOPIC)
+    .load()
+    .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+
+  def flattenDataframe(df: DataFrame): DataFrame = {
+
+    val fields = df.schema.fields
+    val fieldNames = fields.map(x => x.name)
+
+    for(i <- 0 to fields.length-1){
+      val field = fields(i)
+      val fieldtype = field.dataType
+      val fieldName = field.name
+      fieldtype match {
+        case arrayType: ArrayType =>
+          val fieldNamesExcludingArray = fieldNames.filter(_!=fieldName)
+          val fieldNamesAndExplode = fieldNamesExcludingArray ++ Array(s"explode_outer($fieldName) as $fieldName")
+          // val fieldNamesToSelect = (fieldNamesExcludingArray ++ Array(s"$fieldName.*"))
+          val explodedDf = df.selectExpr(fieldNamesAndExplode:_*)
+          return flattenDataframe(explodedDf)
+        case structType: StructType =>
+          val childFieldnames = structType.fieldNames.map(childname => fieldName +"."+childname)
+          val newfieldNames = fieldNames.filter(_!= fieldName) ++ childFieldnames
+          val renamedcols = newfieldNames.map(x => (col(x.toString()).as(x.toString().replace(".", "_"))))
+          val explodedf = df.select(renamedcols:_*)
+          return flattenDataframe(explodedf)
+        case _ =>
+      }
+    }
+    df
+  }
+
+  val schema = new StructType(Array(StructField("tickers", ArrayType(StructType(Array(StructField("name", StringType), StructField("price", IntegerType)))))))
+  val df2 = df.withColumn("valueData", from_json(col("value"), schema)).select("valueData.*")
+
+  val finalDf = flattenDataframe(df2).groupBy("tickers_name").agg(avg("tickers_price").as("avg_price"))
+
+  while(true) {
+    finalDf.show
+    println("Time :: " + DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now) + "\n\n\n")
+    Thread.sleep(30000)
+  }
+
+
+}
+
+/*
+* val df = spark
+  .readStream
+  .format("kafka")
+  .option("kafka.bootstrap.servers", "localhost:9092")
+  .option("startingOffsets", "earliest")
+  .option("subscribe", "test")
+  .load()
+
+  val df2 = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)").as[(String, String)]
+  import org.apache.spark.sql.types.StringType
+  import org.apache.spark.sql.types.IntegerType
+  import org.apache.spark.sql.types.StructField
+  import org.apache.spark.sql.types.StructType
+  import org.apache.spark.sql.types.ArrayType
+
+  val schema = new StructType(Array(StructField("tickers", ArrayType(StructType(Array(StructField("name", StringType), StructField("price", IntegerType)))))))
+  val df3 = df2.withColumn("valueData", from_json(col("value"), schema)).select("valueData.*")
+
+  val df4 = df3.withColumn("stock1", col("tickers").getItem(0)).withColumn("stock2", col("tickers").getItem(1)).withColumn("stock3", col("tickers").getItem(2))
+  val df5 = df4.select("stock1", "stock2", "stock3")
+  df5.writeStream.format("console").start()
+*
+* */
---
+++ src/main/scala/producer/StockProducer.scala
@@ -0,0 +1,91 @@
+package producer
+
+//import org.apache.spark.sql.SparkSession
+import com.google.gson.Gson
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
+import java.util.Properties
+import scala.util.Random
+
+case class Stock(name: String, price: Int)
+case class Message(tickers: Array[Stock])
+
+object StockProducer extends App {
+
+  val BROKER_LIST = "localhost:9092"
+  val TOPIC = "test"
+  val BATCH_SIZE = 10
+  val TIME_TO_WAIT_MILLIS: Long = 100
+
+  val random = new Random()
+  val gson = new Gson
+
+  val AMZN_MEAN_PRICE = 1902
+  val AMZN_MIN_PRICE = getStockMinPrice(AMZN_MEAN_PRICE)
+  val AMZN_MAX_PRICE = getStockMaxPrice(AMZN_MEAN_PRICE)
+
+  val MSFT_MEAN_PRICE = 107
+  val MSFT_MIN_PRICE = getStockMinPrice(MSFT_MEAN_PRICE)
+  val MSFT_MAX_PRICE = getStockMaxPrice(MSFT_MEAN_PRICE)
+
+  val AAPL_MEAN_PRICE = 215
+  val AAPL_MIN_PRICE = getStockMinPrice(AAPL_MEAN_PRICE)
+  val AAPL_MAX_PRICE = getStockMaxPrice(AAPL_MEAN_PRICE)
+
+  val stockMap: Map[Int, String] = Map(1 -> "AMZN", 2 -> "MSFT", 3 -> "AAPL")
+  val priceMap: Map[Int, (Int, Int)] = Map(1 -> (AMZN_MIN_PRICE, AMZN_MAX_PRICE), 2 -> (MSFT_MIN_PRICE, MSFT_MAX_PRICE),
+    3 -> (AAPL_MIN_PRICE, AAPL_MAX_PRICE))
+
+  val properties = new Properties()
+  properties.put("bootstrap.servers", BROKER_LIST)
+  //properties.put("batch.size", BATCH_SIZE)
+  //properties.put("linger.ms", TIME_TO_WAIT_MILLIS)
+  properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+  val producer = new KafkaProducer[String, String](properties)
+
+  def getStockMinPrice(meanPrice: Int): Int = {
+    (0.9 * meanPrice).toInt
+  }
+
+  def getStockMaxPrice(meanPrice: Int): Int = {
+    (1.1 * meanPrice).toInt
+  }
+
+  def generateStockMessage(): String = {
+    val numTickers = 1 + random.nextInt(3) // 1, 2 or 3
+    val stockKeysVisited = scala.collection.mutable.Set[Int]()
+    val stocks = new Array[Stock](numTickers)
+    for (i <- 1 to numTickers) {
+      var randomStockKey = 1 + random.nextInt(3)
+      while (stockKeysVisited.contains(randomStockKey)) {
+        randomStockKey = 1 + random.nextInt(3)
+      }
+      stockKeysVisited.add(randomStockKey)
+      val stockName: String = stockMap.get(randomStockKey) match {
+        case None => ""
+        case Some(s) => s
+      }
+      val stockPriceRange: Int = priceMap.get(randomStockKey).toSeq(0)._2 - priceMap.get(randomStockKey).toSeq(0)._1
+      val randomStockPrice: Int = priceMap.get(randomStockKey).toSeq(0)._1 + random.nextInt(stockPriceRange + 1)
+      stocks(i - 1) = Stock(stockName, randomStockPrice)
+    }
+    gson.toJson(Message(stocks))
+  }
+
+  def sendBatch(): Unit = {
+    var keyCounter = 1
+    while (true) {
+      for (i <- 1 to 10) {
+        val msg = new ProducerRecord[String, String](TOPIC, keyCounter.toString, generateStockMessage())
+        println("Sending message :: " + msg)
+        producer.send(msg)
+        keyCounter += 1
+      }
+      Thread.sleep(1000)
+    }
+  }
+
+  sendBatch()
+}
